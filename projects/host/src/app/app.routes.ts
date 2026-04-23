427a6d1e9ddc84fa85cc5b307192060620b7ac55
Tsironis Ioannis <gtsironis8@gmail.com>
2026-04-23 15:49:47 +0300

    added micro frontends using native federation

--- projects/host/src/app/app.routes.ts
+++ projects/host/src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+// Add this route:
+{
+    path: 'remote',
+    loadComponent: () => loadRemoteModule('remote', './Component').then((m) => m.RemoteComponent),
+  },
+
+];
